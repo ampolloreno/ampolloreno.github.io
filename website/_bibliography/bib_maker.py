c52a3ad3f4812cd65e134e4d5ae0e7c87bfef1fb
Anthony Polloreno <anthony.polloreno@gmail.com>
2018-01-10 21:41:31 -0800

    bib_maker better

--- website/_bibliography/bib_maker.py
+++ website/_bibliography/bib_maker.py
@@ -18,6 +18,7 @@ new_bib = text
 with open(ARXIV, 'r') as filehandler:
     for arxiv_link in filehandler:
         doi, field, status = arxiv_link.strip().split(' ')
+        print("Retrieving {}!".format(doi))
         if status == '.':
             status = ""
         query = arxiv.query(doi)
@@ -25,7 +26,7 @@ with open(ARXIV, 'r') as filehandler:
         authors = query[0]['authors']
         title = query[0]['title'].replace("\n ", "")
         new_bib += "\n"
-        description = str(arxiv.query(doi)[0]['published_parsed'].tm_year) + '_' + authors[0].replace(' ', '')
+        description = str(arxiv.query(doi)[0]['published_parsed'].tm_year) + '_' + str(arxiv.query(doi)[0]['published_parsed'].tm_mon) + '_' +  authors[0].replace(' ', '')
         new_bib += bibtex.format(description, title, " & ".join(authors), link, field, status)
 
 with open(BIB, 'w') as filehandler:
