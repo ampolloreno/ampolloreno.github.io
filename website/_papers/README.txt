06efa2057d0a916e770d3a40dc242b25f2754ce3
apolloreno <apolloreno@canopyfog.com>
2019-10-29 20:08:13 -0600

    Build site at 2019-10-30 02:08:13 UTC

---
+++ website/_papers/README.txt
@@ -0,0 +1,1 @@
+The file format, as of now, is to save the paper as first author-title_{doi/arxiv}.pdf. This doesn't always work, because there might be slashes. Therefore replace all slashes with __.
---
+++ website/_papers/make_refs.py
@@ -0,0 +1,32 @@
+import os
+import re
+
+blocks = []
+d = '.'
+dirs = [os.path.join(d, o) for o in os.listdir(d) if os.path.isdir(os.path.join(d, o))]
+for dir in dirs:
+    category = os.path.basename(dir)
+    for paper in os.listdir(os.path.join(dir, 'todo')):
+        paper = '.'.join(paper.split('.')[:-1]) #remove the file extension
+        author = paper.split('-')[0]
+        title_doiorarxiv = "-".join(paper.split('-')[1:])
+        doiorarxiv = title_doiorarxiv.split('_')[-1]
+        doi = True if '|' in doiorarxiv else False
+
+        if re.match('\d{4}.', doiorarxiv):
+            arxiv = True
+        else:
+            arxiv = False
+
+        doiorarxiv = doiorarxiv.replace('|', '/')
+        if doi:
+            ref = f'doi: {doiorarxiv}'
+        elif arxiv:
+            ref = f'arxiv: {doiorarxiv}'
+        else:
+            ref = ''
+        lines = [f'{paper}:', f'tags: {category}', ref]
+        blocks.append("\n\t".join(lines))
+
+with open('../_gitbib/example/refs.yaml', 'w') as handler:
+    handler.write(('\n'.join(blocks)))
